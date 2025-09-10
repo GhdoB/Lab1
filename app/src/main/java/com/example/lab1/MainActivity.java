e0c0118bbed151467b2dc212c200b840842454b8
GhodB <vinegub@gmail.com>
2025-09-10 22:01:41 +0300

    Branch changes: new button

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tvTextChange;
     Button changeColourButton;
+    Button changeBackColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +40,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //Branch changes
-
+        this.changeBackColor = findViewById(R.id.btnChangeBackground);
+        changeBackColor.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                tvTextChange.setBackgroundColor(Color.RED);
+            }
+        });
     }
 
     public void onBtnTextChangeClk(View view) {
